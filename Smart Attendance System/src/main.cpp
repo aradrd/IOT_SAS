bab7cfd760562521be3878fb8541952ac1502663
Arad Reder <reder.arad@campus.technion.ac.il>
2023-08-15 13:55:25 +0300

    main.cpp now has a working setup that flashes the display with a logo, dumps rfid when a card is near and shows keypresses on serial.

--- Smart Attendance System/src/main.cpp
+++ Smart Attendance System/src/main.cpp
@@ -1,18 +1,97 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <I2CKeyPad.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Screen consts.
+const uint8_t DISPLAY_WIDTH = 128;
+const uint8_t DISPLAY_HEIGHT = 32;
+const uint8_t DISPLAY_RST = -1; // -1 means shared (whatever that means).
+const uint8_t DISPLAY_ADDR = 0x3C;
+
+// RFID consts.
+const uint8_t RFID_RST = 2;
+const uint8_t RFID_SS = 5;
+
+// Keypad consts (and non-const).
+const uint8_t KEYPAD_ADDR = 0x20;
+char keymap[17] = "DCBA#9630852*741";
+
+// Glob inits.
+MFRC522 mfrc522(RFID_SS, RFID_RST);
+I2CKeyPad keyPad(KEYPAD_ADDR);
+Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT, &Wire, DISPLAY_RST);
+
+uint8_t i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  // Init I2C.
+  Wire.begin();
+  Wire.setClock(400000);
+
+  // Init keypad.
+  if (keyPad.begin() == false) {
+    Serial.println("Can't communicate to keypad.");
+  }
+  else {
+    keyPad.loadKeyMap(keymap);
+    Serial.println(keyPad.getKeyPadMode());
+  }
+
+  // Init SPI and RFID.
+  while(!Serial);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  delay(4);
+  mfrc522.PCD_DumpVersionToSerial();
+
+  // Init display.
+  if (!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDR)) {
+    Serial.println(F("SSD1306 allocation failed"));
+  }
+  else {
+    display.display();
+  }
+}
+
+void keypadLoop() {
+  Wire.requestFrom(KEYPAD_ADDR, (uint8_t) 1);
+  if (keyPad.isPressed())
+  {
+    char ch = keyPad.getChar();     // note we want the translated char
+    Serial.print(ch);
+    Serial.println(" pressed.");
+  }
+}
+
+void displayLoop() {
+  display.invertDisplay(i % 20 >= 10);
+}
+
+void RFIDLoop() {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  i++;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  keypadLoop();
+  displayLoop();
+  RFIDLoop();
+
+  delay(100);
 }
