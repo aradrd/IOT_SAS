6f8964c19409d0af9d33c2f182ae78f07bd36cb7
Elina Levi <elinalevi2@gmail.com>
2023-08-29 10:34:16 +0300

    added google sheet class

---
+++ Smart Attendance System/src/GoogleSheet.h
@@ -0,0 +1,82 @@
+#ifndef GOOGLE_SHEET_H
+#define GOOGLE_SHEET_H
+
+#include <HTTPClient.h>
+#include <WiFi.h>
+
+String GOOGLE_SCRIPT_ID = "AKfycbwE-wRd4-k9RimSp_svSTjKdhQbHha_pTppacQrqA0_s8QRCHWLDjZIl7IhHYOTQRB3";
+
+class GoogleSheet{
+public:
+    GoogleSheet(const char* ssid, const char *password) : ssid(ssid), password(password) {};
+    GoogleSheet(const GoogleSheet&) = default;
+    GoogleSheet& operator=(const GoogleSheet&) = delete;
+
+    void init(){
+        init_wifi();
+    }
+    
+    void readDataFromGoogleSheet(){
+        if (WiFi.status() == WL_CONNECTED) {
+            HTTPClient http;
+            String url = "https://script.google.com/macros/s/" + GOOGLE_SCRIPT_ID + "/exec";
+            Serial.println("Making a request");
+            Serial.println(url);
+            http.begin(url.c_str()); //Specify the URL and certificate
+            http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+            int httpCode = http.GET();
+            String payload;
+            if (httpCode > 0) { //Check for the returning code
+                payload = http.getString();
+                Serial.println(httpCode);
+                Serial.println(payload);
+                //display1.println(payload);
+            }
+            else {
+                Serial.println("Error on HTTP request");
+            }
+            http.end();
+        }
+    }
+
+    void post_data(){
+        String urlFinal = "https://script.google.com/macros/s/" + GOOGLE_SCRIPT_ID + "/exec";
+
+        Serial.println("POST data to spreadsheet:");
+        Serial.println(urlFinal);
+        HTTPClient http;
+        http.begin(urlFinal.c_str());
+        http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+        // Specify content-type header
+        http.addHeader("Content-Type", "application/json");
+        // Data to send with HTTP POST
+        String httpRequestData = "{\"something\":\"elina\"}";           
+        // Send HTTP POST request
+        int httpResponseCode = http.POST(httpRequestData);
+        Serial.print("HTTP Status Code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+    }
+
+
+private:
+    const char* ssid;
+    const char* password;
+
+    void init_wifi() {
+        WiFi.mode(WIFI_STA);
+        //WiFi.begin(ssid, password);
+        WiFi.begin(ssid);
+        Serial.print("Connecting to WiFi ..");
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            Serial.print('.');
+            delay(1000);
+        }
+        Serial.println(WiFi.localIP());
+    }
+
+};
+
+#endif
--- Smart Attendance System/src/main.cpp
+++ Smart Attendance System/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
 
 #include <Display.h>
 #include <IOTKeypad.h>
+#include <GoogleSheet.h>
 
 const uint8_t DISPLAY1_WIDTH = 128;
 const uint8_t DISPLAY1_HEIGHT = 64;
 
 const uint8_t DISPLAY2_WIDTH = 128;
 const uint8_t DISPLAY2_HEIGHT = 32;
-
 
 const uint8_t I2C_SDA_2 = 33;
 const uint8_t I2C_SCL_2 = 32;
@@ -26,71 +24,14 @@ const uint8_t RFID_SS = 5;
 const char* ssid = "TechPublic";
 const char* password = "12345678";
 
-String GOOGLE_SCRIPT_ID = "AKfycbwE-wRd4-k9RimSp_svSTjKdhQbHha_pTppacQrqA0_s8QRCHWLDjZIl7IhHYOTQRB3";
-
 // Glob inits.
 MFRC522 mfrc522(RFID_SS, RFID_RST);
 Display display1(&Wire, DISPLAY1_WIDTH, DISPLAY1_HEIGHT);
 Display display2(&Wire1, DISPLAY2_WIDTH, DISPLAY2_HEIGHT);
 IOTKeypad keypad(display2);
+GoogleSheet googleSheet(ssid, password);
 
 uint8_t i = 0;
-
-void readDataFromGoogleSheet(){
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    String url = "https://script.google.com/macros/s/" + GOOGLE_SCRIPT_ID + "/exec";
-    Serial.println("Making a request");
-    Serial.println(url);
-    http.begin(url.c_str()); //Specify the URL and certificate
-    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-    int httpCode = http.GET();
-    String payload;
-    if (httpCode > 0) { //Check for the returning code
-      payload = http.getString();
-      Serial.println(httpCode);
-      Serial.println(payload);
-      display1.println(payload);
-    }
-    else {
-      Serial.println("Error on HTTP request");
-    }
-    http.end();
-  }
-}
-
-void post_data(){
-  String urlFinal = "https://script.google.com/macros/s/" + GOOGLE_SCRIPT_ID + "/exec";
-
-  Serial.println("POST data to spreadsheet:");
-  Serial.println(urlFinal);
-  HTTPClient http;
-  http.begin(urlFinal.c_str());
-  http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-  // Specify content-type header
-  http.addHeader("Content-Type", "application/json");
-  // Data to send with HTTP POST
-  String httpRequestData = "{\"something\":\"elina\"}";           
-  // Send HTTP POST request
-  int httpResponseCode = http.POST(httpRequestData);
-  Serial.print("HTTP Status Code: ");
-  Serial.println(httpResponseCode);
-  String payload = http.getString();
-  Serial.println(payload);
-}
-
-void init_wifi() {
-  WiFi.mode(WIFI_STA);
-  //WiFi.begin(ssid, password);
-  WiFi.begin(ssid);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-}
 
 void setup() {
   Serial.begin(9600);
@@ -111,10 +52,9 @@ void setup() {
   mfrc522.PCD_Init();
   mfrc522.PCD_DumpVersionToSerial();
 
-  // Init Wifi
-  // init_wifi();
-  //readDataFromGoogleSheet();
-  // post_data();
+  googleSheet.init();
+  googleSheet.readDataFromGoogleSheet();
+  googleSheet.post_data();
 }
 
 void RFIDLoop() {
